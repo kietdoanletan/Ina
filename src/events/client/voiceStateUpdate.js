e118339dd0c0f867a95319f5debf835bcc91bb7c
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-07-25 16:03:54 +0000

    feat(voiceStateUpdate): send message on disconnect

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -1,8 +1,14 @@
+const { EmbedBuilder } = require('discord.js');
 module.exports = async (client, o, n) => {
     if (o.member.id !== client.user.id || n.member.id !== client.user.id) return;
     if (n.channelId === null && o.channelId !== null) {
         const player = client.kazagumo.players.get(o.guild.id);
         if (player && (player.state !== 5 && player.state !== 6)) client.kazagumo.destroyPlayer(o.guild.id);
+        const embed = new EmbedBuilder()
+            .setDescription('*I was disconnected from the voice channel, and destroyed the player automatically.*')
+            .setColor(client.config.color)
+            .setFooter(client.config.footer);
+        client.channels.cache.get(player.textId)?.send({ embeds: [embed] });
         for (const msg of player.cleanup) {
             if (msg.interaction && msg.interaction.replied) {
                 await msg.interaction.deleteReply().catch(() => null);
