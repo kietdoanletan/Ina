db4d4cc50f4536d0017f81e21f8d52876387cfb1
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-07-25 11:29:20 +0000

    feat(cleanup): cleanup on player disconnect

--- src/events/client/voiceStateUpdate.js
+++ src/events/client/voiceStateUpdate.js
@@ -3,5 +3,14 @@ module.exports = async (client, o, n) => {
     if (n.channelId === null && o.channelId !== null) {
         const player = client.kazagumo.players.get(o.guild.id);
         if (player && (player.state !== 5 && player.state !== 6)) client.kazagumo.destroyPlayer(o.guild.id);
+        for (const msg of player.cleanup) {
+            if (msg.interaction && msg.interaction.replied) {
+                await msg.interaction.deleteReply().catch(() => null);
+            } else if (msg.replied && msg.replied == true) {
+                await msg.deleteReply().catch(() => null);
+            } else {
+                await msg.delete().catch(() => null);
+            }
+        }
     }
 };
