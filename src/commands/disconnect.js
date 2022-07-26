9b5cdfa7519cfad95b0ddbd796f5ea4f0f5fd283
Thaddeus Kuah <thaddeuskkr@gmail.com>
2022-07-26 05:37:54 +0000

    feat(disconnect): add a disconnect command

---
+++ src/commands/disconnect.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, EmbedBuilder, escapeMarkdown } = require('discord.js');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('disconnect')
+        .setDescription('Stops the player if there is anything playing, and disconnects the player from the voice channel.'),
+    permissions: [],
+    checks: ['IN_VC', 'SAME_VC'],
+    async run (client, interaction, player) {
+        if (!player) {
+            const embed = new EmbedBuilder()
+                .setAuthor({ name: 'Error' })
+                .setDescription('There is no player for this server.')
+                .setColor(client.config.errorColor)
+                .setFooter(client.config.footer);
+            if (interaction.guild.members.cache.get(client.user.id).voice.channelId) interaction.guild.members.cache.get(client.user.id).voice.disconnect();
+            return interaction.reply({ embeds: [embed], fetchReply: true }).then(msg => setTimeout(() => { msg.delete(); }, 10000));
+        }
+        const embed = new EmbedBuilder()
+            .setAuthor({ name: 'Disconnected' })
+            .setDescription(`Disconnected from **${escapeMarkdown(client.channels.cache.get(player?.voiceId).name)}**.`)
+            .setColor(client.config.color)
+            .setFooter(client.config.footer);
+        await player?.destroy();
+        if (interaction.guild.members.cache.get(client.user.id).voice.channelId) interaction.guild.members.cache.get(client.user.id).voice.disconnect();
+        interaction.reply({ embeds: [embed], fetchReply: true }).then(msg => setTimeout(() => { msg.delete(); }, 10000));
+    }
+};
